80cdecf40d52158497eb24c9038419fd74180b36
Christian Podda <poddachristian94@gmail.com>
2022-03-03 17:38:12 +0100

    Richiesta dei valori

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-console.log(
-    'JS ok!'
-);
+
+// JS console check
+console.log('JS ok!');
+
+// Richiesta nome
+const Name = prompt('Ciao! Come ti chiami?');
+
+// Log nome
+console.log(Name);
+
+// Richiesta cognome
+const Surname = prompt('E il tuo cognome?');
+
+// Log cognome
+console.log(Surname);
+
+// Richiesta colore preferito
+const FavoredColor = prompt('Adesso dimmi il tuo colore preferito!');
+
+// Log colore preferito
+console.log(FavoredColor);
